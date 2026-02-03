21721191311433bb86407e29cae7174b4e9b12a2
garv <gbaid@fordham.edu>
2026-02-03 12:39:53 -0500

    Customized My Valentine

--- script.js
+++ script.js
@@ -57,12 +57,12 @@ const messages = [
     "Are you sure?",
     "Really sure??",
     "Are you positive?",
-    "Pookie please...",
+    "Honey please...",
     "Just think about it!",
     "If you say no, I will be really sad...",
     "I will be very sad...",
     "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
+    "Ok fine, I will stop asking!...",
     "Just kidding, say yes please! ❤️"
 ];
 
